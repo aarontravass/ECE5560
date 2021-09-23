3eed7203a740e99d624a0ed1dd8363b5b01f22f2
aarontravass <aaron.travass@hotmail.com>
2021-09-23 23:42:57 +0530

    working sol

--- Assignment_1/hill_cipher/main.cpp
+++ Assignment_1/hill_cipher/main.cpp
@@ -6,12 +6,12 @@
 #define forn_rev(i,n)for(ll i=(n)-1;i>=0;i--)
 #define for1n(i,n) for(ll i=1;i<=(n);i++)
 #define print2D(arr) forn(i,arr.size()) { forn(j,arr[0].size()) { cout<<arr[i][j]<<" "; } cout<<endl;}
-#define vec2D vector<vector<int>>
+#define vec2D vector<vector<ll>>
 using namespace std;
 
-vec2D getCofactor(vec2D mat, vec2D temp, int p, int q, int n)
+vec2D getCofactor(vec2D mat, vec2D temp, ll p, ll q, ll n)
 {
-    int i = 0, j = 0;
+    ll i = 0, j = 0;
     forn(row,n)
     {
         forn(col,n)
@@ -30,14 +30,14 @@ vec2D getCofactor(vec2D mat, vec2D temp, int p, int q, int n)
     return temp;
 }
 
-int determinantOfMatrix(vec2D mat, int n,int len)
+ll determinantOfMatrix(vec2D mat, ll n,ll len)
 {
-    int D = 0;
+    ll D = 0;
     if (n == 1)
         return mat[0][0];
 
-    vec2D temp(len,vector<int>(len));
-    int sign = 1;
+    vec2D temp(len,vector<ll>(len));
+    ll sign = 1;
     forn(f,n)
     {
         temp=getCofactor(mat, temp, 0, f, n);
@@ -50,25 +50,25 @@ int determinantOfMatrix(vec2D mat, int n,int len)
 
 vec2D convertStrToSQMat(string s)
 {
-    int sz=ceil(sqrt(s.length()));
-    vec2D mat(sz,vector<int>(sz,0));
-    int cnt=0;
+    ll sz=ceil(sqrt(s.length()));
+    vec2D mat(sz,vector<ll>(sz,0));
+    ll cnt=0;
     forn(i,sz) forn(j,sz) mat[i][j]=(s[(cnt++)%s.length()]-'A');
     return mat;
 }
 
-int add(int a,int b)
+ll add(ll a,ll b)
 {
     return (a+b)%26;
 }
-int mul(int a,int b)
+ll mul(ll a,ll b)
 {
     return (a*b)%26;
 }
 
 vec2D matMul(vec2D a,vec2D b)
 {
-    vec2D ans(a.size(),vector<int>(b[0].size(),0));
+    vec2D ans(a.size(),vector<ll>(b[0].size(),0));
     forn(i,a.size())
     {
         forn(j,b[0].size())
@@ -84,7 +84,7 @@ vec2D matMul(vec2D a,vec2D b)
 
 vec2D plainToMat(string plaintext)
 {
-    vec2D ans(plaintext.size(),vector<int>(1,0));
+    vec2D ans(plaintext.size(),vector<ll>(1,0));
     forn(i,plaintext.size()) ans[i][0]=plaintext[i]-'A';
     return ans;
 }
@@ -95,13 +95,13 @@ string matToString(vec2D m)
     return ans;
 }
 
-int findInDet(int det)
+ll findInDet(ll det)
 {
     forn(i,det) if(mul(det,i)==1)
         return i;
     return -1;
 }
-vec2D mulMatbyNum(vec2D mat,int num)
+vec2D mulMatbyNum(vec2D mat,ll num)
 {
     forn(i,mat.size())
     forn(j,mat[0].size())
@@ -117,14 +117,14 @@ vec2D mulMatbyNum(vec2D mat,int num)
 
 vec2D adjoint(vec2D A)
 {
-    vec2D adj(A.size(),vector<int>(A.size(),0));
-    vec2D temp(A.size(),vector<int>(A.size(),0));
+    vec2D adj(A.size(),vector<ll>(A.size(),0));
+    vec2D temp(A.size(),vector<ll>(A.size(),0));
     if (A[0].size() == 1)
     {
         adj[0][0] = 1;
         return adj;
     }
-    int sign = 1;
+    ll sign = 1;
     forn(i,A[0].size())
     {
         forn(j,A[0].size())
@@ -137,17 +137,18 @@ vec2D adjoint(vec2D A)
     return adj;
 }
 
-vec2D inverse(vec2D adj,int det)
+vec2D inverse(vec2D adj,ll det)
 {
     return mulMatbyNum(adj,findInDet(det));
 }
+
+
 int main()
 {
-    int n;
     string key,plaintext;
     cin>>key>>plaintext;
-    int sz=ceil(sqrt(key.length()));
-    int added_chars=0;
+    ll sz=ceil(sqrt(key.length()));
+    ll added_chars=0;
     while(1)
     {
         if(plaintext.size()%sz!=0)
@@ -158,11 +159,10 @@ int main()
         else
             break;
     }
-    cout<<plaintext<<endl;
     transform(key.begin(), key.end(), key.begin(), ::toupper);
     transform(plaintext.begin(), plaintext.end(), plaintext.begin(), ::toupper);
-    vector<vector<int>> mat=convertStrToSQMat(key);
-    int det=determinantOfMatrix(mat,mat.size(),mat.size());
+    vector<vector<ll>> mat=convertStrToSQMat(key);
+    ll det=determinantOfMatrix(mat,mat.size(),mat.size());
 
     if(det==0)
     {
@@ -174,11 +174,10 @@ int main()
         return 0;
     }
     string full_enc="",full_dec="";
-    for(int l=0; l<plaintext.size(); l+=sz)
+    for(ll l=0; l<plaintext.size(); l+=sz)
     {
-        cout<<l<<endl;
         string s="";
-        for(int j=l; j<l+sz; j++)
+        for(ll j=l; j<l+sz; j++)
             s+=plaintext[j];
         vec2D plainMat=plainToMat(s);
         vec2D ans=matMul(mat,plainMat);
